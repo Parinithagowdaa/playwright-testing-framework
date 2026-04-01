66cd0b8d5b563a00a32632b862e169362111aec1
shwethaah <shwetha.hagedal@winwire.com>
2026-04-01 13:42:52 +0530

    UI updates - removal of additional tabs/texts

--- playwright-latest-codegen.spec.ts
+++ playwright-latest-codegen.spec.ts
@@ -2,4 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://advantageonlineshopping.com/#/');
+  await page.getByRole('link', { name: 'POPULAR ITEMS' }).click();
+  await page.getByRole('link', { name: 'View Details' }).nth(2).click();
+  await page.locator('e-sec-plus-minus div').nth(3).click();
 });
